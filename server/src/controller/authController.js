7e08e7563a8b3b38676f6a1c69999262b2f9c782
ragurithvik <ragurithvik@gmail.com>
2025-09-20 01:19:45 +0530

    expiry time removed in token

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -29,7 +29,7 @@ export const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "2h" });
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
     res.json({ token });
   } catch (err) {
     res.status(500).json({ error: err.message });

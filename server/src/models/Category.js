07dbdde8b22e723c649718619ead8a15fdf1f513
ragurithvik <ragurithvik@gmail.com>
2025-09-21 00:44:42 +0530

    images added

--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 //new schema for category
 const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true, image_url: { type: String, required: false } }
+  name: { type: String, required: true },
+  image_url: { type: String, required: false }
 });
 
 export default mongoose.model("Category", categorySchema);
